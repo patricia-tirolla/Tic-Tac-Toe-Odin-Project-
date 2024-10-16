e03bbf4b975188884be241d89e6e3e21ce842b86
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-10-16 15:51:26 +0200

    Refactor code using Factory Functions
    The code now is more readable and safe, with
    less code in the global scope

--- main.js
+++ main.js
@@ -1,102 +1,94 @@
 // Gameboard object
 const Gameboard = {
     myBoard: ["", "", "", "", "", "", "", "", ""],
-}
-
-// Player 1 object
-let Player1 = {
-    name: "x",
-    score: 0
-}
-// Player 2 object
-let Player2 = {
-    name: "o",
-    score: 0
-}
-
-let currentPlayer = Player1;
-
-// Gameflow object
-
-// Show gameboard
-// player = Player1
-// first player plays
-// myBoard list update
-// check if ther's a winner
-// if yes, increase the score, update score display and reset the board
-// if no, switch to Player2
-// start again
-
-// ---------- CRIAR TESTES PARA AS FUNÇÕES
-function cleanGameboard() {
-    Gameboard.myBoard = ["", "", "", "", "", "", "", "", ""];
-}
-
-function increaseScore() {
-    if (winConditions(currentPlayer.name) === true) {
-        currentPlayer.score++;
-        cleanGameboard();
+    cleanBoard: function () {
+        this.myBoard = ["", "", "", "", "", "", "", "", ""];
     }
 }
 
-function winConditions(playerName) {
-    if (
-        Gameboard.myBoard[0] === playerName &&
-        Gameboard.myBoard[1] === playerName &&
-        Gameboard.myBoard[2] === playerName ||
-
-        Gameboard.myBoard[3] === playerName &&
-        Gameboard.myBoard[4] === playerName &&
-        Gameboard.myBoard[5] === playerName ||
-
-        Gameboard.myBoard[6] === playerName &&
-        Gameboard.myBoard[7] === playerName &&
-        Gameboard.myBoard[8] === playerName ||
-
-        Gameboard.myBoard[0] === playerName &&
-        Gameboard.myBoard[3] === playerName &&
-        Gameboard.myBoard[6] === playerName ||
-
-        Gameboard.myBoard[1] === playerName &&
-        Gameboard.myBoard[4] === playerName &&
-        Gameboard.myBoard[7] === playerName ||
-
-        Gameboard.myBoard[2] === playerName &&
-        Gameboard.myBoard[5] === playerName &&
-        Gameboard.myBoard[8] === playerName ||
-
-        Gameboard.myBoard[0] === playerName &&
-        Gameboard.myBoard[4] === playerName &&
-        Gameboard.myBoard[8] === playerName ||
-
-        Gameboard.myBoard[2] === playerName &&
-        Gameboard.myBoard[4] === playerName &&
-        Gameboard.myBoard[6] === playerName
-    ) {
-        return true;
-    } else {
-        return false;
+function createPlayer(name) {
+    return {
+        name,
+        score: 0
     }
 }
 
-function switchPlayer() {
-    if (currentPlayer === Player1) {
-        currentPlayer = Player2;
-    } else {
-        currentPlayer = Player1;
+function createGameFlow() {
+    const player1 = createPlayer("x");
+    const player2 = createPlayer("o");
+    return {
+        currentPlayer: player1,
+        player1: player1,
+        player2: player2,
+        playerFlow(dataIndex) {
+            this.changeIndex(dataIndex);
+            if (this.winConditions(this.currentPlayer.name)) {
+                this.increaseScore();
+                Gameboard.cleanBoard();
+            } else {
+                this.switchPlayer();
+            }
+        },
+
+        changeIndex(index) {
+            Gameboard.myBoard[index] = this.currentPlayer.name;
+        },
+
+        switchPlayer() {
+            if (this.currentPlayer === player1) {
+                this.currentPlayer = player2;
+            } else {
+                this.currentPlayer = player1;
+            }
+        },
+
+        increaseScore() {
+            if (this.winConditions(this.currentPlayer.name) === true) {
+                this.currentPlayer.score++;
+                Gameboard.cleanBoard();
+            }
+        },
+
+        winConditions(playerName) {
+            if (
+                Gameboard.myBoard[0] === playerName &&
+                Gameboard.myBoard[1] === playerName &&
+                Gameboard.myBoard[2] === playerName ||
+
+                Gameboard.myBoard[3] === playerName &&
+                Gameboard.myBoard[4] === playerName &&
+                Gameboard.myBoard[5] === playerName ||
+
+                Gameboard.myBoard[6] === playerName &&
+                Gameboard.myBoard[7] === playerName &&
+                Gameboard.myBoard[8] === playerName ||
+
+                Gameboard.myBoard[0] === playerName &&
+                Gameboard.myBoard[3] === playerName &&
+                Gameboard.myBoard[6] === playerName ||
+
+                Gameboard.myBoard[1] === playerName &&
+                Gameboard.myBoard[4] === playerName &&
+                Gameboard.myBoard[7] === playerName ||
+
+                Gameboard.myBoard[2] === playerName &&
+                Gameboard.myBoard[5] === playerName &&
+                Gameboard.myBoard[8] === playerName ||
+
+                Gameboard.myBoard[0] === playerName &&
+                Gameboard.myBoard[4] === playerName &&
+                Gameboard.myBoard[8] === playerName ||
+
+                Gameboard.myBoard[2] === playerName &&
+                Gameboard.myBoard[4] === playerName &&
+                Gameboard.myBoard[6] === playerName
+            ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
 
-function changeIndex(index) {
-    Gameboard.myBoard[index] = currentPlayer.name;
-}
-
-function playFlow(dataIndex) {
-    changeIndex(dataIndex);
-    if (winConditions(currentPlayer.name)) {
-        increaseScore();
-        cleanGameboard();
-    } else {
-        switchPlayer();
-    }
-}
+const gameFlow = createGameFlow();
--- tests.js
+++ tests.js
@@ -2,22 +2,22 @@
 function testIncreaseScore(player) {
     console.log(player.score);
     Gameboard.myBoard = [player.name, player.name, player.name, "", "", "", "", "", ""];
-    increaseScore(currentPlayer);
+    gameFlow.increaseScore(gameFlow.currentPlayer);
     console.log(player.score);
 }
-testIncreaseScore(currentPlayer);
+testIncreaseScore(gameFlow.currentPlayer);
 
 // test switch players
 function testSwitchPlayer() {
     console.log("Testing swish player")
-    console.assert(currentPlayer === Player1);
-    console.debug(currentPlayer);
-    switchPlayer();
-    console.assert(currentPlayer === Player2);
-    console.debug(currentPlayer);
-    switchPlayer();
-    console.assert(currentPlayer === Player1);
-    console.debug(currentPlayer);
+    console.assert(gameFlow.currentPlayer === gameFlow.player1);
+    console.debug(gameFlow.currentPlayer);
+    gameFlow.switchPlayer();
+    console.assert(gameFlow.currentPlayer === gameFlow.player2);
+    console.debug(gameFlow.currentPlayer);
+    gameFlow.switchPlayer();
+    console.assert(gameFlow.currentPlayer === gameFlow.player1);
+    console.debug(gameFlow.currentPlayer);
 }
 testSwitchPlayer()
 
@@ -25,50 +25,76 @@ testSwitchPlayer()
 function testWinConditions() {
     console.log("testing win first line")
     Gameboard.myBoard = ["x", "x", "x", "", "", "", "", "", ""];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 
     console.log("testing win second line")
     Gameboard.myBoard = ["", "", "", "x", "x", "x", "", "", ""];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 
     console.log("testing win third line")
     Gameboard.myBoard = ["", "", "", "", "", "", "x", "x", "x"];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 
     console.log("testing win first column")
     Gameboard.myBoard = ["x", "", "", "x", "", "", "x", "", ""];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 
     console.log("testing win second column")
     Gameboard.myBoard = ["", "x", "", "", "x", "", "", "x", ""];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 
     console.log("testing win third column")
     Gameboard.myBoard = ["", "", "x", "", "", "x", "", "", "x"];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 
     console.log("testing win first cross")
     Gameboard.myBoard = ["x", "", "", "", "x", "", "", "", "x"];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 
     console.log("testing win second cross")
     Gameboard.myBoard = ["", "", "x", "", "x", "", "x", "", ""];
-    console.assert(winConditions("x"));
+    console.assert(gameFlow.winConditions("x"));
 }
 testWinConditions();
 
 // test play game flow
 function testChangeIndex() {
-    changeIndex(0);
+    gameFlow.changeIndex(0);
     console.log(Gameboard.myBoard);
-    changeIndex(5);
+    gameFlow.changeIndex(5);
     console.log(Gameboard.myBoard);
-    changeIndex(1);
+    gameFlow.changeIndex(1);
     console.log(Gameboard.myBoard);
-    changeIndex(6);
-    changeIndex(2);
+    gameFlow.changeIndex(6);
+    console.log(Gameboard.myBoard);
+    gameFlow.changeIndex(2);
     console.log(Gameboard.myBoard);
 }
 testChangeIndex()
-cleanGameboard();
+Gameboard.cleanBoard();
 
+//test game flow
+function testGameFlow() {
+    console.log("testing game flow")
+
+    console.log("\ttesting the game starts with gameFlow.player1")
+    console.assert(gameFlow.currentPlayer === gameFlow.player1, "current player is not player 1");
+
+    gameFlow.playerFlow(0);
+
+    console.log("\ttesting if player is switched to player2")
+    console.assert(gameFlow.currentPlayer === gameFlow.player2, "current player is not player2");
+
+    console.log("\ttesting the score is not updating")
+    console.assert(gameFlow.currentPlayer.score === 0);
+
+    gameFlow.playerFlow(4);
+    gameFlow.playerFlow(1);
+    gameFlow.playerFlow(5);
+    gameFlow.playerFlow(2);
+
+    console.log("\ttesting the score is updating")
+    console.assert(gameFlow.currentPlayer.score === 2);    
+};
+testGameFlow();
+Gameboard.cleanBoard();
